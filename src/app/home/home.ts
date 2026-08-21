c72dcd400eb737aebab46bcf1b1651725ec68355
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-21 17:38:27 +0200

    add housing component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from "@angular/core";
+import { HousingLocation } from "../housing-location/housing-location";
 
 @Component({
   selector: "app-home",
-  imports: [],
+  imports: [HousingLocation],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section>
+      <app-housing-location />
     </section>
   `,
   styleUrl: "./home.css",
