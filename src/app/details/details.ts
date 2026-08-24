83c36d3f1e217c8a0268365f2b63163fb0270764
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-24 12:41:05 +0200

    add apply form

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import { HousingService } from "../housing.service";
 import { HousingLocationInfo } from "../housinglocation";
 
 @Component({
   selector: "app-details",
-  imports: [],
+  imports: [ReactiveFormsModule],
   template: `
     <article>
       <img
@@ -30,6 +31,20 @@ import { HousingLocationInfo } from "../housinglocation";
           </li>
         </ul>
       </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName" />
+
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName" />
+
+          <label for="email">Email</label>
+          <input id="email" type="text" formControlName="email" />
+          <button type="submit" class="primary">Apply now</button>
+        </form>
+      </section>
     </article>
   `,
   styleUrl: "./details.css",
@@ -39,9 +54,23 @@ export class Details {
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocationInfo | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
     this.housingLocation =
       this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? "",
+      this.applyForm.value.lastName ?? "",
+      this.applyForm.value.email ?? "",
+    );
   }
 }
